452db46357036cb7aadc45ebea41ebbb5efac6ac
VolodymyrRiz <v.v.rizun@gmail.com>
2024-02-15 16:43:42 +0200

    ht6-7

--- nnij/query_1..sql
+++ nnij/query_1..sql
@@ -1,1 +1,6 @@
 -- Знайти 5 студентів із найбільшим середнім балом з усіх предметів.
+SELECT st.student, AVG(m.mark) as averageMark
+FROM students_nnij AS st
+LEFT JOIN marks_nnij AS m ON st.id=m.students_nnij_id
+GROUP BY st.id
+HAVING st.id < 6
--- nnij/query_2.sql
+++ nnij/query_2.sql
@@ -3,3 +3,5 @@ SELECT st.student, Subj.subject_ AS Subject_, AVG(m.mark) as averageMark
 FROM students_nnij AS st
 LEFT JOIN marks_nnij AS m ON st.id=m.students_nnij_id
 LEFT JOIN subjects_nnij AS Subj ON Subj.id=m.subjects_nnij_id
+WHERE st.id>=1 AND st.id<=30
+GROUP BY Subj.id
